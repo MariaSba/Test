82bf650ada0acf2a2245d0964068c02a5664190a
MariaSba <57985141+MariaSba@users.noreply.github.com>
2021-04-07 14:43:01 +0200

    nn

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -17,7 +17,7 @@ public class JavaApplication5 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.print("haha"); 
+        System.out.print("haha  "); 
     }
     
 }
